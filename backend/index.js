74c0ab4e890fb73645522ab7adfbbb1a0bab6b6c
Karan Jamwal <karanjamwal2004@gmail.com>
2025-11-26 20:20:58 +0530

    Install backend dependencies and initial setup

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectToDB } from "./utils/db.js";
+
+dotenv.config({});
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  connectToDB();
+  console.log(`Server started on port ${PORT}`);
+});
